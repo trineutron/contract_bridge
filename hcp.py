4edca1020138f248f152933c241140d43d23ab7f
trineutron <programgoo@gmail.com>
2022-11-09 20:19:10 +0900

    分割方法を変更

--- hcp.py
+++ hcp.py
@@ -1,6 +1,3 @@
-import re
-
-
 def hcp(s):
     res = 0
     for c in s:
@@ -20,11 +17,12 @@ with open('Library.asc', 'rt', encoding='utf-8') as fin:
 
 count = [[0] * 8 for _ in range(41)]
 for line in data:
-    s = re.split(' |:', line)
+    s, tricks = line.split(':')
+    s = s.split()
     for i in range(4):
         totalhcp = hcp(s[i]) + hcp(s[(i + 2) % 4])
         count[totalhcp][0] += 1
-        trick = int(s[4][(2 - i) % 4], 16)
+        trick = int(tricks[(2 - i) % 4], 16)
         if i % 2 == 0:
             trick = 13 - trick
         for j in range(1, trick - 5):
@@ -34,7 +32,7 @@ for i in range(41):
         if count[i][0] == 0:
             break
         count[i][j] /= count[i][0]
-    print("{:2d}".format(i), end=' ')
+    print(f"{i:2d}", end=' ')
     for j in range(1, 8):
-        print("{:.5f}".format(count[i][j]), end=' ')
+        print(f"{count[i][j]:.5f}", end=' ')
     print(count[i][0])
